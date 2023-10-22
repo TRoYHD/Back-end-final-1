d11dd614e54646a2849ac4ce798ff89611bcb6b7
TRoYHD <dark_angle.2010@hotmail.com>
2023-10-22 19:43:28 +0300

    add seeders for product image

---
+++ seeders/20231019120000-create-productsImage.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const productImages = [
+      {
+        product_id: 1,
+        image: 'https://res.cloudinary.com/drq2xwi3o/image/upload/v1697625193/E-Commerce/2_g7egru.webp',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        product_id: 2,
+        image: 'https://res.cloudinary.com/drq2xwi3o/image/upload/v1697625193/E-Commerce/2_g7egru.webp',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      // Add more objects with your custom URLs as needed
+    ];
+
+    await queryInterface.bulkInsert('ProductImages', productImages, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('ProductImages', null, {});
+  }
+};
