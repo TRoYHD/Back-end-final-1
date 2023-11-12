8dcf02009ef1928daa5531a30c329dc94de84727
TRoYHD <dark_angle.2010@hotmail.com>
2023-11-12 20:38:20 +0200

    fixed the cookies

--- build/middlewares/setUserId.middleware.js
+++ build/middlewares/setUserId.middleware.js
@@ -8,10 +8,17 @@ const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const errors_1 = require("./errors");
 const http_status_1 = __importDefault(require("http-status"));
 const setUserId = (req, res, next) => {
-    const { token } = req.cookies;
-    if (!token)
+    // Assuming you store the token in local storage under the key 'token'
+    const token = localStorage.getItem('token');
+    if (!token) {
         next(new errors_1.CustomError('Unauthenticated', http_status_1.default.UNAUTHORIZED));
+        return; // Make sure to return after calling next to stop further execution
+    }
     const user = jsonwebtoken_1.default.decode(token);
+    if (!user || typeof user.id !== 'number') {
+        next(new errors_1.CustomError('Invalid token', http_status_1.default.UNAUTHORIZED));
+        return; // Make sure to return after calling next to stop further execution
+    }
     req.userId = user.id;
     next();
 };
